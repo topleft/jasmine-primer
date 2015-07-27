4679b63e546c76865ed6fd8790fe0464049df51e
topleft <pete.james.jeffryes@gmail.com>
2015-07-27 14:00:55 -0600

    finished 5 test specs all passing

---
+++ main.js
@@ -0,0 +1,24 @@
+exports.outputHelloWorld = function () {
+  return "Hello, world!";
+};
+
+exports.calculate = function (amt){
+
+
+  if (amt > 30) {
+    var d = (amt - 30)*0.03;
+    return d+2.2;
+  }
+  else if (amt > 20) {
+    c = (amt - 20)*0.05;
+    return c+1.7;
+  }
+  else if (amt > 10) {
+    b = (amt - 10)*0.07;
+    return b+1;
+  }
+  else {
+    return amt*0.10;
+  }
+
+};
--- spec/Spec.js
+++ spec/Spec.js
@@ -1,8 +1,31 @@
 var code = require('../main.js');
 
 
-describe('', function() {
-  it('', function() {
-
+describe("Hello World", function() {
+  it("says 'hello, world!' when ran", function() {
+    expect(code.outputHelloWorld()).toEqual("Hello, world!");
   });
+});
+
+describe('Tax Calculator', function(){
+
+  it('should tax 10% on the first $10', function() {
+    expect(code.calculate(1)).toEqual(0.1);
+    expect(code.calculate(10)).toEqual(1);
+  });
+
+  it('should tax 7% on the second $10', function(){
+    expect(code.calculate(15)).toEqual(1.35);
+  });
+
+    it('should tax 5% on the third $10', function(){
+    expect(code.calculate(25)).toEqual(1.95);
+  });
+
+    it('should tax 3% on everything over $30', function(){
+    expect(code.calculate(40)).toEqual(2.50);
+  });
+
+
+
 });
